c9c4a16b995ba0883a77130d4550f8b4af7ca564
adi274903 <73401497+adi274903@users.noreply.github.com>
2025-04-11 04:08:53 +0000

    Update asr_trainer.py

--- IDL-HW4/hw4lib/trainers/asr_trainer.py
+++ IDL-HW4/hw4lib/trainers/asr_trainer.py
@@ -381,7 +381,7 @@ class ASRTrainer(BaseTrainer):
             # Default config (greedy search)
             recognition_config = {
                 'num_batches': 5,
-                'beam_width': 1,
+                'beam_width': 3,
                 'temperature': 1.0,
                 'repeat_penalty': 1.0,
                 'lm_weight': 0.0,
